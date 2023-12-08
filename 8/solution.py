9fa7f3e4d9d249c518543cf98c447911a6b289c2
Kyle Rysdam <kyle@rysdam.org>
2023-12-08 01:58:20 -0500

    Fix comments

--- 8/solution.py
+++ 8/solution.py
@@ -4,7 +4,6 @@ def parse_fork(fork: str) -> tuple:
         fork = fork.replace(superfluous, '')
     return tuple(fork.split())
 
-# A single path from AAA to ZZZ is simple...
 def steps_from_AAA_to_ZZZ(forks: dict, instructions: str) -> int:
     """How many instruction steps from AAA to ZZZ?"""
     # Start at AAA
@@ -20,26 +19,24 @@ def steps_from_AAA_to_ZZZ(forks: dict, instructions: str) -> int:
                 position = forks[position][1]
     return count
 
-# But following multiple paths simultaneously is not efficient.
-# Instead, we can find the pattern of **Zs from each starting point,
-# and use those to find the first step on which all paths are at a **Z.
 def steps_from_As_to_Zs(forks: dict, instructions: str) -> int:
     """How many instruction steps from all **As to all **Zs?"""
     starting_points = [k for k in forks.keys() if k[-1] == 'A']
     # The paths from each starting point, up to the first repeat
-    print("Making paths...")
     cycles = [path_until_repeat(forks, instructions, start) for start in starting_points]
-    #for c in cycles:
-    #    print(c)
-    # Empirically, these cycles all work like this:
+
+    # In the general case, any element of a cycle could end with Z,
+    # and worse still, the cycles might not hit these **Zs at the end of the instructions.
+    # However, the puzzle is simpler than the instructions imply.
+
+    # Empirically, the cycles all work like this:
     # - Step 0 is the starting point, which ends with A
     # - Steps 1 to the end are the cycle, which has a prime length
     # - The last step, and only the last step, ends with Z
     # Therefore, the first step at which all paths are at a Z is
     # the product of (len - 1) for each cycle,
     # all multiplied by the number of instructions.
-    for c in cycles:
-        print(len(c), c)
+
     count = 1
     for cycle in cycles:
         count *= (len(cycle) - 1)
@@ -50,8 +47,8 @@ def path_until_repeat(forks: dict, instuctrions: str, start: str) -> list:
     """What is the pattern of states from the start?"""
     # Start at the start
     position = start
-    # Follow the instructions
     path = [position]
+    # Follow the instructions
     while True:
         for inst in instructions:
             if inst == 'L':
