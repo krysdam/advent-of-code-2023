53f238cdd538f3ba3f941dc3e0211da2e296365e
Kyle Rysdam <kyle@rysdam.org>
2023-12-09 00:34:33 -0500

    Use recursion instead of build all rows together

--- 9/solution.py
+++ 9/solution.py
@@ -2,29 +2,14 @@ def pull_numbers(s: str) -> list:
     """Pull the numbers out of the given string of number."""
     return [int(x) for x in s.split()]
 
-def build_differences_pyramid(seq: list) -> list:
-    """A pyramid of Nth differences, down to a row of all zeros."""
-    # Start with the sequence itself.
-    pyramid = [seq]
-    # Build rows of differences until we get a row of all zeros.
-    while not all(x == 0 for x in pyramid[-1]):
-        last_row = pyramid[-1]
-        differences = [b - a for a, b in zip(last_row, last_row[1:])]
-        pyramid.append(differences)
-    return pyramid
-
 def extrapolate_forwards(seq: list) -> int:
     """Extrapolate the next value of the polynomial sequence."""
-    # Build a pyramid of Nth differences.
-    pyramid = build_differences_pyramid(seq)
-    # Starting from the row of zeros, consruct one more value for each row.
-    for r, row in enumerate(reversed(pyramid)):
-        if r == 0:
-            row.append(0)
-        else:
-            row.append(row[-1] + pyramid[-r][-1])
-    # Return the last value of the sequence itself (row 0).
-    return pyramid[0][-1]
+    # If the sequence is all zeros, the next value is zero.
+    if all(x == 0 for x in seq):
+        return 0
+    # Else, add the next value of this sequence's first differences.
+    differences = [b - a for a, b in zip(seq, seq[1:])]
+    return seq[-1] + extrapolate_forwards(differences)
     
 if __name__ == '__main__':
     total1 = 0
