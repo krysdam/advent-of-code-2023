863099a2caf4acf85cadfecdeabf7ba4dc6d0ec0
Kyle Rysdam <kyle@rysdam.org>
2023-12-12 02:24:36 -0500

    Clean up code, especially lists vs tuples

--- 12/solution.py
+++ 12/solution.py
@@ -1,53 +1,59 @@
-def parse_record(record: str) -> tuple:
+def parse_record(conditions: str) -> tuple:
     """Parse a line of the puzzle input into (binary-record, [nums])."""
-    record, nums = record.split()
-    nums = [int(x) for x in nums.split(',')]
-    return record, nums
+    conditions, nums = conditions.split()
+    nums = tuple([int(x) for x in nums.split(',')])
+    return conditions, nums
+
+def expand_record(record: tuple, factor: int) -> tuple:
+    """Expand a record and nums by a factor, per the problem."""
+    conditions, nums = record
+    conditions = '?'.join([conditions] * factor)
+    nums = nums * factor
+    return conditions, nums
 
 # Memoize this.
 # Otherwise, it keeps recalculating the same couple tail-cases.
 import functools
 @functools.lru_cache(maxsize=None)
-def how_many_possibilities_dynamic(record: str, nums: tuple) -> int:
+def how_many_possibilities_dynamic(conditions: str, nums: tuple) -> int:
     """How many ways can these nums describe this record?"""
     # Base cases:
     # If there's not enough room for the numbers, there are no possibilities.
-    if len(record) < sum(nums) + len(nums) - 1:
+    if len(conditions) < sum(nums) + len(nums) - 1:
         return 0
     # If the record claims '#' but there are no nums, there are no possibilities.
-    if '#' in record and nums == ():
+    if '#' in conditions and nums == ():
         return 0
     # Assuming those are OK, an empty record or empty nums means one possibility.
-    if len(record) == 0:
+    if len(conditions) == 0:
         return 1
     if nums == ():
         return 1
     # Recursive case.
     # The decision is: does the next number start at this character?
-    ch = record[0]
+    ch = conditions[0]
     n = nums[0]
     # If the next char is a '.', then it can't start here.
     if ch == '.':
-        return how_many_possibilities_dynamic(record[1:], nums)
+        return how_many_possibilities_dynamic(conditions[1:], nums)
     # If the next char is a '#', then it must start here.
     if ch == '#':
-        # Also, the next n chars must not be '.'
-        if '.' in record[:n]:
+        # Forbid '.' inside the number.
+        if '.' in conditions[:n]:
             return 0
-        # Furthermore, the char after the number must be a buffer (not #)
-        if len(record) > n and record[n] == '#':
+        # Require a buffer after the number.
+        if len(conditions) > n and conditions[n] == '#':
             return 0
-        # Cut off the buffer while we're at it.
-        return how_many_possibilities_dynamic(record[n+1:], nums[1:])
-    # If the next char is a '?', that's basically either '.' or '#'
+        # Cut of the number and the buffer.
+        return how_many_possibilities_dynamic(conditions[n+1:], nums[1:])
+    # If the next char is a '?', then it can start here or not.
     if ch == '?':
-        record_rest = record[1:]
-        # Option 1: the ? is really a .
-        count1 = how_many_possibilities_dynamic(record_rest, nums)
-        # Option 2: the ? is really a #
+        record_rest = conditions[1:]
+        # Option 1: the '?' is really a '.'
+        count1 = how_many_possibilities_dynamic('.' + record_rest, nums)
+        # Option 2: the '?' is really a '#'
         count2 = how_many_possibilities_dynamic('#' + record_rest, nums)
         return count1 + count2
-
 
 if __name__ == '__main__':
     records = []
@@ -55,29 +61,18 @@ if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         for line in f:
             line = line.strip()
-
             records.append(parse_record(line))
 
     # Part 1: how many possibilities are there for each record?
     sum1 = 0
-    for record, nums in records:
-        sum1 += how_many_possibilities_dynamic(record, tuple(nums))
-        #print()
+    for record in records:
+        sum1 += how_many_possibilities_dynamic(*record)
     print("Part 1:", sum1)
 
     # Part 2: each record becomes five of itself
     multiplier = 5
     sum2 = 0
-    records = sorted(records, key=lambda x: x[0].count('?'))
-    new_records = []
-    for record, nums in records:
-        while '..' in record:
-            record = record.replace('..', '.')
-        new_records.append((record, nums))
-    records = new_records
-    for i, (record, nums) in enumerate(records):
-        print(i, i / len(records), record, nums)
-        record = '?'.join([record] * multiplier)
-        nums = nums * multiplier
-        sum2 += how_many_possibilities_dynamic(record, tuple(nums))
+    for record in records:
+        record = expand_record(record, multiplier)
+        sum2 += how_many_possibilities_dynamic(*record)
     print("Part 2:", sum2)
