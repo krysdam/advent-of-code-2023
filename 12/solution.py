cf3e6fef8841bd98545f624e8eff7ed5bbdd606a
Kyle Rysdam <kyle@rysdam.org>
2023-12-12 02:13:48 -0500

    Memoize recursive function, correct and fast

--- 12/solution.py
+++ 12/solution.py
@@ -4,30 +4,33 @@ def parse_record(record: str) -> tuple:
     nums = [int(x) for x in nums.split(',')]
     return record, nums
 
-
-def how_many_possibilities_dynamic(record: str, nums: list) -> int:
+# Memoize this.
+# Otherwise, it keeps recalculating the same couple tail-cases.
+import functools
+@functools.lru_cache(maxsize=None)
+def how_many_possibilities_dynamic(record: str, nums: tuple) -> int:
     """How many ways can these nums describe this record?"""
-    #print(record, nums)
-    # Base cases.
-    # If there's not enough room, there are zero possibilities.
+    # Base cases:
+    # If there's not enough room for the numbers, there are no possibilities.
     if len(record) < sum(nums) + len(nums) - 1:
         return 0
-    # If the record claims '#' but there are no nums, there are zero possibilities.
-    if '#' in record and nums == []:
+    # If the record claims '#' but there are no nums, there are no possibilities.
+    if '#' in record and nums == ():
         return 0
-    # Assuming there's room, an empty record or empty nums means one possibility.
+    # Assuming those are OK, an empty record or empty nums means one possibility.
     if len(record) == 0:
         return 1
-    if nums == []:
+    if nums == ():
         return 1
     # Recursive case.
-    # The only choice is: does the next number start at this character?
+    # The decision is: does the next number start at this character?
+    ch = record[0]
     n = nums[0]
-    # If the next char is a '.', then it can't start here
-    if record[0] == '.':
+    # If the next char is a '.', then it can't start here.
+    if ch == '.':
         return how_many_possibilities_dynamic(record[1:], nums)
-    # If the next char is a '#', then it must start here
-    if record[0] == '#':
+    # If the next char is a '#', then it must start here.
+    if ch == '#':
         # Also, the next n chars must not be '.'
         if '.' in record[:n]:
             return 0
@@ -37,7 +40,7 @@ def how_many_possibilities_dynamic(record: str, nums: list) -> int:
         # Cut off the buffer while we're at it.
         return how_many_possibilities_dynamic(record[n+1:], nums[1:])
     # If the next char is a '?', that's basically either '.' or '#'
-    if record[0] == '?':
+    if ch == '?':
         record_rest = record[1:]
         # Option 1: the ? is really a .
         count1 = how_many_possibilities_dynamic(record_rest, nums)
@@ -58,7 +61,7 @@ if __name__ == '__main__':
     # Part 1: how many possibilities are there for each record?
     sum1 = 0
     for record, nums in records:
-        sum1 += how_many_possibilities_dynamic(record, nums)
+        sum1 += how_many_possibilities_dynamic(record, tuple(nums))
         #print()
     print("Part 1:", sum1)
 
@@ -76,5 +79,5 @@ if __name__ == '__main__':
         print(i, i / len(records), record, nums)
         record = '?'.join([record] * multiplier)
         nums = nums * multiplier
-        sum2 += how_many_possibilities_dynamic(record, nums)
+        sum2 += how_many_possibilities_dynamic(record, tuple(nums))
     print("Part 2:", sum2)
