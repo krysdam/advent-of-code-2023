bb46eedc08d426cc1439f67fee582026ec6f8eed
Kyle Rysdam <kyle@rysdam.org>
2023-12-14 00:33:25 -0500

    Solve day 14 part 1, very slow for part 2

---
+++ 14/solution.py
@@ -0,0 +1,83 @@
+
+# Memoize this.
+import functools
+@functools.lru_cache(maxsize=None)
+def roll_row_east(row: str) -> str:
+    """Roll all round rocks west on this row."""
+    segments = []
+    segment = ''
+    area = row[0] in '.O'
+    for ch in row:
+        if area:
+            if ch in '.O':
+                segment += ch
+            else:
+                segments.append(segment)
+                segment = ch
+                area = False
+        else:
+            if ch in '.O':
+                segments.append(segment)
+                segment = ch
+                area = True
+            else:
+                segment += ch
+    segments.append(segment)
+    segments = [''.join(sorted(segment)) for segment in segments]
+    return ''.join(segments)
+
+def roll_platform_east(platform: list) -> list:
+    """Roll all round rocks west on this platform."""
+    return [roll_row_east(row) for row in platform]
+
+def rotate_clockwise(platform: list) -> list:
+    """Rotate the given platform clockwise."""
+    return [''.join(row) for row in zip(*platform[::-1])]
+
+def spin_cycle(platform: list) -> list:
+    """Roll the plaform north, then west, then south, then east."""
+    # Put North to the east, and roll
+    platform = rotate_clockwise(platform)
+    platform = roll_platform_east(platform)
+    # Put West to the east, and roll
+    platform = rotate_clockwise(platform)
+    platform = roll_platform_east(platform)
+    # Put South to the east, and roll
+    platform = rotate_clockwise(platform)
+    platform = roll_platform_east(platform)
+    # Put East to the east, and roll
+    platform = rotate_clockwise(platform)
+    platform = roll_platform_east(platform)
+    return platform
+
+def find_weight_on_north(platform: list) -> int:
+    height = len(platform)
+    total = 0
+    for i, row in enumerate(platform):
+        total += (height - i) * row.count('O')
+    return total
+
+if __name__ == '__main__':
+    platform = []
+
+    with open('example.txt', 'r') as f:
+        for line in f:
+            line = line.strip()
+
+            platform.append(line)
+
+    # Part 1: roll north, then find weight
+    platform1 = platform.copy()
+    platform1 = rotate_clockwise(platform1)
+    platform1 = roll_platform_east(platform1)
+    platform1 = rotate_clockwise(platform1)
+    platform1 = rotate_clockwise(platform1)
+    platform1 = rotate_clockwise(platform1)
+    print("Part 1:", find_weight_on_north(platform1))
+
+    # Part 2: many spin cycles
+    platform2 = platform.copy()
+    for r in range(1000000000):
+        if r % 1000000 == 0:
+            print(r)
+        platform2 = spin_cycle(platform2)
