ea55c1c2a9983d6c6c9e214dc58ab969e85680a3
Kyle Rysdam <kyle@rysdam.org>
2023-12-16 00:31:15 -0500

    Comments and cleanup for readability

--- 16/solution.py
+++ 16/solution.py
@@ -1,44 +1,55 @@
 def process_grid(grid: list, starting_beam: tuple) -> set:
+    """Find how many tiles are visited by the beam starting at the given beam."""
+    # Bounds of the grid
     maxy, maxx = len(grid), len(grid[0])
 
+    beams = [starting_beam]
+
     def add_beam(y, x, dy, dx):
-        #print("about to append", y, x, dy, dx)
+        # Add the given beam to the queue of active beams,
+        # If the new beam is in-bounds and not a repeat.
         if 0 <= y < maxy and 0 <= x < maxx:
             beam = (y, x, dy, dx)
             if beam not in beams:
                 beams.append(beam)
-            #print("appended")
+
+    # Track which tiles are touched.
     visited = set()
 
-    beams = [starting_beam]
-
+    # Traverse the queue of active beams.
     for beam in beams:
         y, x, dy, dx = beam
-
         visited.add((y, x))
-        ch = grid[y][x]
-        match ch:
+
+        # React to the current tile.
+        match grid[y][x]:
             case '.':
+                # Sail on.
                 add_beam(y + dy, x + dx, dy, dx)
             case '/':
+                # Swap dy and dx, and invert.
                 add_beam(y - dx, x - dy, -dx, -dy)
             case '\\':
+                # Swap dy and dx.
                 add_beam(y + dx, x + dy, dx, dy)
             case '|':
+                # If we're moving vertically, sail on.
                 if dx == 0:
                     add_beam(y + dy, x + dx, dy, dx)
+                # If we're moving horizontally, split.
                 elif dy == 0:
                     add_beam(y+1, x, 1, 0)
                     add_beam(y-1, x, -1, 0)
             case '-':
+                # If we're moving vertically, split.
                 if dx == 0:
                     add_beam(y, x+1, 0, 1)
                     add_beam(y, x-1, 0, -1)
+                # If we're moving horizontally, sail on.
                 elif dy == 0:
                     add_beam(y, x + dx, dy, dx)
+    # Number of tiles touched by any beam.
     return len(visited)
-
-
 
 if __name__ == '__main__':
     grid = []
@@ -55,11 +66,13 @@ if __name__ == '__main__':
     # Part 2: Find optimal start
     possibilities = []
     for y in range(len(grid)):
-        print(y)
+        # Start along the left edge, heading right.
         possibilities.append(process_grid(grid, (y, 0, 0, 1)))
+        # Start along the right edge, heading left.
         possibilities.append(process_grid(grid, (y, len(grid[0])-1, 0, -1)))
     for x in range(len(grid[0])):
-        print(x)
+        # Start along the top edge, heading down.
         possibilities.append(process_grid(grid, (0, x, 1, 0)))
+        # Start along the bottom edge, heading up.
         possibilities.append(process_grid(grid, (len(grid)-1, x, -1, 0)))
     print(max(possibilities))   
