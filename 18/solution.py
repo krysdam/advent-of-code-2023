493edb99b733e9ceb2697f797fa89eb2995e1579
Kyle Rysdam <kyle@rysdam.org>
2023-12-18 16:19:24 -0500

    Refactor: add perimeter, and small naming changes

--- 18/solution.py
+++ 18/solution.py
@@ -1,30 +1,38 @@
 DIRECTION_NAMES_TO_DYDX = {'U': (-1, 0), 'R': (0, 1), 'D': (1, 0), 'L': (0, -1)}
 DIRECTION_NUMS_TO_DYDX = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
-def find_trench_points(instructions: list) -> dict:
-    """The corners on the path."""
+def motions_to_vertices(instructions: list) -> dict:
+    """Given a list of motions, find the vertices of the resulting polygon."""
     y, x = 0, 0
-    trench = []
+    vertices = []
     for dy, dx, dist in instructions:
         y += dy * dist
         x += dx * dist
-        trench.append((y, x))
-    return trench
+        vertices.append((y, x))
+    return vertices
 
 def determinant(x1, x2, y1, y2) -> int:
     """The determinant of a 2x2 matrix."""
     return x1 * y2 - x2 * y1
 
-def shoelace_area(points: list) -> int:
-    """The area of a polygon, using the shoelace formula.
-    
-    points: the corners of the polygon"""
+def shoelace_area(vertices: list) -> int:
+    """Given the vertices of a polygon, find its area."""
     area = 0
-    for i in range(len(points)):
-        x1, y1 = points[i]
-        x2, y2 = points[(i+1) % len(points)]
+    for i in range(len(vertices)):
+        x1, y1 = vertices[i]
+        x2, y2 = vertices[(i+1) % len(vertices)]
         area += determinant(x1, x2, y1, y2)
     return abs(area) / 2
+
+def perimeter(vertices: list) -> int:
+    """The number of tiles on the edge of a polygon."""
+    perimeter = 0
+    for i in range(len(vertices)):
+        y1, x1 = vertices[i]
+        y2, x2 = vertices[(i+1) % len(vertices)]
+        segment = abs(x1 - x2) + abs(y1 - y2)
+        perimeter += segment
+    return perimeter
 
 def area(points: list) -> int:
     """The number of tiles in or on a polygon.
@@ -32,15 +40,12 @@ def area(points: list) -> int:
     points: the corners of the polygon"""
     # Start with the shoelace area.
     area = shoelace_area(points)
-    # But now we're missing part of each tile on the edge.
-    # How much of those tiles?
-    # Half of the ones on the edge, and either 1/4 or 3/4 of the ones on the corners.
-    # The total winding number is 1, so these 1/4 and 3/4 tiles cancel out.
-    # So we just need to add 1/2 of the tiles on the edge, plus 1 (the winding number).
-    for p1, p2 in zip(points, points[1:] + [points[0]]):
-        segment = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-        area += segment / 2
-    area += 1
+    # But now we're missing part of each tile on the path itself.
+    # Each tile on an edge contributes half its area.
+    # Tiles on convex corners contribute 3/4, and concave ones contribute 1/4.
+    # The winding number is 1, so we have 4 more convex corners than concave.
+    # So the total area of these partial tiles is (perimeter/2) + 1.
+    area += perimeter(points) / 2 + 1
     return area
 
 def color_to_instruction(color: str) -> tuple:
@@ -68,10 +73,10 @@ if __name__ == '__main__':
 
     # Part 1: instructions dir and dist
     instructions1 = [(dy, dx, dist) for dy, dx, dist, color in instructions]
-    trench = find_trench_points(instructions1)
-    print("Part 1:", area(trench))
+    vertices = motions_to_vertices(instructions1)
+    print("Part 1:", area(vertices))
 
     # Part 2: instructions are hidden in 'color'
     instructions2 = [color_to_instruction(color) for dy, dx, dist, color in instructions]
-    trench = find_trench_points(instructions2)
-    print("Part 2:", str(int(area(trench))))
+    vertices = motions_to_vertices(instructions2)
+    print("Part 2:", str(int(area(vertices))))
